f7a8b5142fa18befeb80e26476b2c5a09106659d
Junaid Niz <junaidaliniz56@gmail.com>
2026-01-10 08:24:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ API_URLS = [
     "https://kamina-otp.up.railway.app/mait/sms",
     "https://kami-broken.up.railway.app/api?type=sms",
     "https://juanidniz-pw-orpin.vercel.app/api/tempotps?type=sms",
+    "https://teamlegend-gamma.vercel.app//api/nmp?type=sms",
+    "https://teamlegend-gamma.vercel.app/api/dgroup?type=sms"*
     
     
 ]
